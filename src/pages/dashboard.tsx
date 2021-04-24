2263cccae4cb6cd98b04153dd2ca4ee139eb4e7a
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-24 01:19:30 -0300

    Chapter 4 -Responsividade - Dashboard Responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
           align="flex-start"
         >
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
