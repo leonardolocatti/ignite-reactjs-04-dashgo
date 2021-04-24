9af79849f11b1c6064048705a3639c289b07f35a
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-23 22:31:06 -0300

    Chapter 4 -Controle de Usuários - Componente pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Form/Sidebar";
+import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
   return (
@@ -121,6 +122,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
