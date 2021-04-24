d9ce953ba3a81872b6c695b1fbca5c184e09ed0e
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-24 00:57:06 -0300

    Chapter 4 -Responsividade - Header Responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Leonardo Locatti</Text>
-        <Text color="gray.300" fontSize="small">
-          leo@llocatti.com.br
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Leonardo Locatti</Text>
+          <Text color="gray.300" fontSize="small">
+            leo@llocatti.com.br
+          </Text>
+        </Box>
+      ) }
 
       <Avatar size="md" name="Leonardo Locatti" src="https://github.com/leonardolocatti.png" />
     </Flex>
