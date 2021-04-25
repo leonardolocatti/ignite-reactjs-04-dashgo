fde421075be6ff3a7190350e285d9242b7455cfa
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-25 17:56:09 -0300

    Chapter 4 -Responsividade - Validação dos Dados

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,20 +1,23 @@
 import { 
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps
 } from "@chakra-ui/react";
+import { FieldError } from 'react-hook-form'
 import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
 const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps> 
-= ({ name, label, ...rest }, ref) => {
+= ({ name, label, error = null, ...rest }, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
       <ChakraInput
@@ -30,6 +33,12 @@ const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps>
         ref={ref}
         {...rest}
         />
+
+        { !! error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
       </FormControl>
   )
 }
