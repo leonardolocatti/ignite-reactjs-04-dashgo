da668c303278aeda2440a958db33a4d8d9c38b72
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-24 23:40:58 -0300

    Chapter 4 -Responsividade - Fluxo de Navegação

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -4,6 +4,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input";
+import Link from "next/link";
 
 export default function CreateUser() {
   return (
@@ -37,7 +38,9 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
