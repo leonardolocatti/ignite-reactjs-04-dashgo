6af32c90ca312d1fadb4aa52899aa047eb6d2fea
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-04-30 22:55:26 -0300

    Chapter 4 -Construindo Aplicação - Trocando de Página e Prefetch de Dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,9 +6,12 @@ import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-  const { data, isLoading, isFetching,error } = useUsers();
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isFetching,error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -69,7 +72,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => (
+                  {data.users.map(user => (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
                         <Checkbox colorScheme="pink" />
@@ -104,9 +107,9 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
